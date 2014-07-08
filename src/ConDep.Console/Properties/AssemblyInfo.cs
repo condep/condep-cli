ed9916e86b039a80d63b81f9c357067f06415d0d
Jon Arild Torresdal <jon@torresdal.net>
2014-07-08 23:29:42 +0200

    Added git files. Tried to clean up refs

--- src/ConDep.Console/Properties/AssemblyInfo.cs
+++ src/ConDep.Console/Properties/AssemblyInfo.cs
@@ -1,11 +1,21 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ConDep.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyCompanyAttribute("ConDep")]
+[assembly: AssemblyProductAttribute("")]
+[assembly: AssemblyCopyrightAttribute("Copyright © ConDep 2014")]
+[assembly: AssemblyVersionAttribute("2.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.0.0.0")]
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("855038ea-7a93-4e16-a59b-9193f82c8292")]
+
+
+
+
