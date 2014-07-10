d9ec889649405bdfcd0afacbd7afd2317434df22
Jon Arild Torresdal <jon@torresdal.net>
2014-07-10 09:27:12 +0200

    Added test project

---
+++ src/ConDep.Console.Tests/CmdFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using ConDep.Console.Deploy;
+using NUnit.Framework;
+
+namespace ConDep.Console.Tests
+{
+    [TestFixture]
+    public class CmdFactoryTests
+    {
+        [Test]
+        public void TestThat_FactoryCanResolveDeploymentCommand()
+        {
+            var factory = new CmdFactory(new[] { "deploy" });
+            Assert.That(factory.Resolve(), Is.InstanceOf<CmdDeployHandler>());
+        }
+
+    }
+}
